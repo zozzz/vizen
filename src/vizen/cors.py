2d2f5e78e13484d2d400bfa5b1341ee52852ac69
Vetési Zoltán <vetesi.zoltan@gmail.com>
2019-04-15 19:43:58 +0200

    feat(cors): redesign a little bit

--- src/vizen/cors.py
+++ src/vizen/cors.py
@@ -6,15 +6,67 @@ from .error import HTTPError
 CORS_ORIGINS = Token("CORS_ORIGINS")
 
 
-def CORS(request: Request, response: Response, origins: CORS_ORIGINS):
-    headers = request.headers
+class CORS:
+    """ Cross-Origin Resource Sharing
 
-    try:
-        origin = headers[b"origin"]
-    except KeyError:
-        response.headers[b"access-control-allow-origin"] = b"*"
-    else:
-        if origin.decode("ASCII") not in origins:
-            raise HTTPError(403)
+    example::
+
+        @Server.on_options("/cors")
+        async def handle_options(cors: CORS):
+            cors.allow_methods(b"POST")
+            cors.allow_headers(b"content-type")
+            cors.max_age(3600)
+            await cors.done()
+
+    Function body is not executed if the allowed origins is not contains, the requested origin
+
+
+    """
+    __slots__ = ("request", "response", "origins")
+
+    request: Inject[Request]
+    response: Inject[Response]
+    origins: Inject[CORS_ORIGINS]
+
+    def __init__(self):
+        headers = self.request.headers
+
+        try:
+            origin = headers[b"origin"]
+        except KeyError:
+            self.response.headers[b"access-control-allow-origin"] = b"*"
         else:
-            response.headers[b"access-control-allow-origin"] = origin
+            if origin.decode("ASCII") not in self.origins:
+                raise HTTPError(403)
+            else:
+                self.response.headers[b"access-control-allow-origin"] = origin
+
+    def allow_methods(self, *methods: bytes) -> None:
+        """ Set ``Access-Control-Allow-Methods`` response header
+
+        example::
+
+            allow_methods(b"GET", b"POST")
+        """
+        self.response.headers[b"access-control-allow-methods"] = b", ".join(methods)
+
+    def allow_headers(self, *headers: bytes) -> None:
+        """ Set ``Access-Control-Allow-Headers`` response header
+
+        example::
+
+            allow_headers(b"content-type", b"origin")
+        """
+        self.response.headers[b"access-control-allow-headers"] = b", ".join(headers)
+
+    def max_age(self, age: int) -> None:
+        """ Set ``Access-Control-Max-Age`` response header
+
+        example::
+
+            max_age(3600)
+        """
+        self.response.headers[b"access-control-max-age"] = str(age).encode("ASCII")
+
+    async def done(self):
+        await self.response.send(b"")
